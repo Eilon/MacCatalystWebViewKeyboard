07967c84741082291a5ba38ce8aad66ca799c425
Eilon Lipton <Eilon@users.noreply.github.com>
2023-03-22 11:17:35 -0700

    Initial Commit

---
+++ MacCatalystWebViewKeyboard/MacCatalystWebViewKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCatalystWebViewKeyboardApp.swift
+//  MacCatalystWebViewKeyboard
+//
+//  Created by Eilon Lipton on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MacCatalystWebViewKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
